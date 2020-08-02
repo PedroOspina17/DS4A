5d07c7df94c7d26fe06c4aac3dd17db9b29ff254
PedroOspina17 <pedroospina17@gmail.com>
2020-08-01 21:31:35 -0500

    Overview progress...

--- App/Components/HorizontalBars.py
+++ App/Components/HorizontalBars.py
@@ -63,3 +63,66 @@ for yd, xd, pos in zip(y_s, df_slice["DPNOM"],df_slice["fetalRatio"]):
                             showarrow=False))
 
 fig.update_layout(annotations=annotations)
+
+
+
+
+######################### General - Overview
+
+
+toplotGeneral = d.perCapDptos.groupby("DPNOM")["fetalRatio"].mean().reset_index().sort_values(by="fetalRatio",ascending=True)
+
+GeneralFig = make_subplots(rows=1, cols=1,vertical_spacing=0.1)
+
+GeneralFig.append_trace(go.Bar(
+    x= toplotGeneral["fetalRatio"],y =toplotGeneral["DPNOM"],#width=5,
+    marker=dict(
+        color='#dcc0dd',
+        line=dict(
+            color='#da5887',
+            width=1)
+    ),
+    name='Household savings, percentage of household disposable income',
+    orientation='h',
+#     width = 5
+), 1, 1)
+
+
+
+
+GeneralFig.update_layout(
+#     title='Household savings & net worth for eight OECD countries',
+    yaxis=dict(
+        showgrid=False,
+        showline=False,
+        showticklabels=True,        
+    ),
+    
+    xaxis=dict(
+        zeroline=False,
+        showline=False,
+        showticklabels=True,
+        showgrid=True,        
+    ),
+    margin=dict(l=0, r=0, t=0, b=0),
+    paper_bgcolor='white',
+    plot_bgcolor='white',
+)
+
+annotations = []
+
+y_s = np.round(toplotGeneral["fetalRatio"]*100, decimals=2)
+# y_nw = np.rint(y_net_worth)
+
+# Adding labels
+for yd, xd, pos in zip(y_s, toplotGeneral["DPNOM"],toplotGeneral["fetalRatio"]):
+    
+    # labeling the bar net worth
+    annotations.append(dict(xref='x', yref='y',
+                            y=xd, x=pos + 0.02,
+                            text=str(yd) + '%',
+                            font=dict(family='Arial', size=12,
+                                      color='#da5887'),
+                            showarrow=False))
+
+GeneralFig.update_layout(annotations=annotations)
